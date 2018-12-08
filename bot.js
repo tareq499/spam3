a7cde12e9e51558db9b9fb35b81f26e14cdcca5e
tareq499 <45701776+tareq499@users.noreply.github.com>
2018-12-07 17:18:52 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+        message.channel.send(`**Holle world Spam , Frame , Credit By Ta , Farm Credit By Ta, ez Farm - Holle world fuck Off**[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -72,7 +72,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+        message.channel.send(`**Holle world spam  , Frame , Credit By Ta , Farm Credit By Ta, ez Farm - Holle world fuck Off **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
